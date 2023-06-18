920347dbb1553c46c7d04f1e1af729b9bd0c2c55
lessa13 <victoria.s.mas@gmail.com>
2023-06-18 12:55:23 +0200

    Pair viernes 16 corregido

---
+++ Pair3YencyVictoria.sql
@@ -0,0 +1,44 @@
+CREATE SCHEMA tienda_zapatillas;
+USE tienda_zapatillas;
+CREATE TABLE empleados 
+(id_empleado INT AUTO_INCREMENT NOT NULL,
+nombre VARCHAR(45) NOT NULL,
+tienda VARCHAR(45) NOT NULL,
+salario INT DEFAULT NULL,
+fecha_de_incorporacion DATE NOT NULL,
+PRIMARY KEY (id_empleado));
+​
+CREATE TABLE clientes
+(id_clientes INT AUTO_INCREMENT NOT NULL,
+nombre VARCHAR(45) NOT NULL,Views
+numero_telefono INT(9) NOT NULL,
+email VARCHAR(45) NOT NULL,
+direccion VARCHAR(45) NOT NULL,
+ciudad VARCHAR(45) NOT NULL,
+provincia VARCHAR(45) NOT NULL,
+pais VARCHAR(45) NOT NULL,
+codigo_postal VARCHAR(45) NOT NULL, 
+PRIMARY KEY (id_clientes));
+CREATE TABLE facturas
+(id_facturas INT AUTO_INCREMENT NOT NULL,
+numero_factura  Varchar(45) NOT NULL,
+fecha DATE NOT NULL,
+zapatilla INT NOT NULL,
+cliente INT NOT NULL,
+empleado INT NOT NULL,
+PRIMARY KEY (id_facturas),
+CONSTRAINT enlacezapatillas 
+	FOREIGN KEY (zapatilla) 
+    REFERENCES zapatillas (id_zapatillas),
+CONSTRAINT enlaceclientes 
+	FOREIGN KEY (cliente) 
+    REFERENCES clientes (id_clientes),
+CONSTRAINT enlaceEmpleados 
+	FOREIGN KEY (empleado) 
+    REFERENCES empleados (id_empleado));
+
+​
+CREATE TABLE zapatillas(id_zapatillas INT NOT NULL AUTO_INCREMENT,
+modelo VARCHAR(45) NOT NULL,
+color VARCHAR(45) NOT NULL,
+PRIMARY KEY (id_zapatillas));
