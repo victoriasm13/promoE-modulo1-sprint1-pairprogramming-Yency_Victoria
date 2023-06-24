04a4501ee50aa14fdcabc0cf80377f8fd6789df9
lessa13 <victoria.s.mas@gmail.com>
2023-06-24 19:14:25 +0200

    actualizacion 24-06

---
+++ PairSQL2306YencyVictoria.sql
@@ -0,0 +1,21 @@
+USE northwind;
+-- Crea una consulta que muestre los diez primeros según su ID.Nos tiene que indicar el nombre
+-- y precio
+
+-- Realiza la misma consulta que antes pero mostrando los 10 últimos productos según su
+-- ID de forma descendiente. 
+
+-- Eliminando entradas duplicadas descubrir que pedidos diferentes se han hecho.
+
+-- Limitar la consulta anterior a los dos primeros resultados
+
+-- Elegir los tres productos que han tenido un mayor coste para la empresa y crear el 
+-- alias Importetoltal
+
+-- Seleccionar el ranbgo 5 a 10 en la lista de Importetotal en orden descendente
+
+-- Lista de categorias con alias NombresCategoria
+
+-- Selecciona usando Between los productos con un precio entre 15 y 50 dolares
+
+-- Selecciona los productos con un precio de 18,19 y 20 dolares
